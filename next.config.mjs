ec281ed3c0c97841632437a6fbdc67d25c35ece2
CAMILOBERMUDEZ1 <juancamilobermudezuribe@gmail.com>
2026-04-15 10:30:58 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	allowedDevOrigins: ['10.1.5.46']
-};
+  output: 'export'
+}
 
-export default nextConfig;
+module.exports = nextConfig
