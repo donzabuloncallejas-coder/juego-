52909b1d90dedd062680f504d8ec77012166dfc7
danielcallejass2025-boop <daniel.callejas.s2025@gmail.com>
2026-04-15 10:49:56 -0500

    act

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	allowedDevOrigins: ['10.1.5.46']
+	allowedDevOrigins: ['10.1.5.46'],
+	output: 'export'
 };
 
 export default nextConfig;
